47626827b368666a93b3c00388aeb8116d972559
DamianFox <d.fossa.91@gmail.com>
2020-03-22 16:30:46 +0100

    Solutions to Task 3 & 4

--- src/TestController.php
+++ src/TestController.php
@@ -9,4 +9,72 @@ use EUFTest\Controller\EUFFetcher;
  */
 class TestController extends EUFFetcher {
 
+	/**
+	 * Get the list of countries
+	 *
+	 * @return array
+	 *   The list of countries
+	 *
+	 */
+	public function getAllCountries() {
+		return json_decode( parent::getRequest('getCountries'), true );;
+	}
+
+	/**
+	 * Get the list of universities by CountryID
+	 *
+	 * @return array
+	 *   The list of universities
+	 *
+	 */
+	public function getUniversitiesByID($countryID) {
+		return json_decode( parent::getRequest('getInstitutions', array("CountryID" => $countryID)), true );
+	}
+
+	/**
+	 * Renders list of universities.
+	 *
+	 * @return string
+	 *   The HTML for the view.
+	 */
+	private function renderUniversities($universities, $ID) {
+		$html = "";
+		foreach($universities as $u){
+			$html .= "<div id=\"collapse".$ID."\" class=\"panel-collapse collapse\">";
+			$html .= "<div class=\"card-body\">".$u["NameInLatinCharacterSet"]." (".$u["CityName"].")</div>";
+			$html .= "</div>";
+		}
+		return $html;
+	}
+
+	/**
+	 * Renders the data from the API request.
+	 *
+	 * @return string
+	 *   The HTML for the view.
+	 */
+	public function render() {
+
+    	$countries = $this->getAllCountries();
+
+    	$html = "";
+    	foreach($countries as $value){
+    		$universities = $this->getUniversitiesByID($value["ID"]);
+    		$number = count($universities);
+    		$html .= "<div class=\"card\" style=\"margin-bottom: 5px;\">";
+    		
+    		$html .= "<div class=\"card-header\">";
+    		$html .= "<a data-toggle=\"collapse\" href=\"#collapse".$value["ID"]."\">";
+    		$html .= $value["CountryName"]." (".$number . ($number == 1 ? " university)" : " universities)");
+    		$html .= "</a>";
+    		$html .= "</div>";
+
+    		$html .= $this->renderUniversities($universities, $value["ID"]);
+    		
+    		$html .= "</div>";
+    	}
+
+    	return $html;
+  	}
+
 }
